f4ba370358ea31dcbe613b77473c11675bdeb5b0
TheDigitalPumpkin <39466847+TheDigitalPumpkin@users.noreply.github.com>
2018-06-25 20:05:55 -0300

    Update fila_p.h

--- TAD_Fila_de_Prioridade/fila_p.h
+++ TAD_Fila_de_Prioridade/fila_p.h
@@ -1,19 +1,13 @@
+//Dupla: Bruno Bacelar e Gabriel Lacerda
+#include "fila.h"
 #ifndef FILA_P_H
 #define FILA_P_H
 
-typedef struct no //Implementaremos a fila de prioridade como uma lista encadeada
-{
-	int info;
-	int prioridade;
-	struct no *prox;
-} No;
-
-No* criar_fila_p();                   //Funcao para criar a fila
-No* novo_no_fila_p(int p, int valor); //Funcao para criar um novo no.
-int fila_p_front(No** cabeca);        //Funcao que retorna o primeiro elemento da fila
-void imprime_fila_p(No** cabeca);     //Funcao que imprime a fila de prioridade
-void fila_p_retirar(No** cabeca);     //Funcao para retirar um elemento da frente da fila
-void fila_p_inserir(No** cabeca, int p, int valor);  //Funcao para inserir um no na fila de acordo com a prioridade
-int fila_p_vazia(No** cabeca);        //Funcao que retorna 1 (true) caso a lista esteja vazia e 0 (false) caso contrario
+void converte_string(char d[]);
+void fila_p_inserir(Fila *f, int cpf_client, int cpf_terceiro, int prioridade, char op, char d[]);  //Função para inserir um nó na fila de acordo com a prioridade
+void retirar_da_fila(Fila* f, int prioridade);
+int fila_vazia(Fila* f, int x);
+Cliente* fila_front(Fila* f, int prioridade);
+int fila_tamanho(Fila* f,int prioridade);
 
 #endif
