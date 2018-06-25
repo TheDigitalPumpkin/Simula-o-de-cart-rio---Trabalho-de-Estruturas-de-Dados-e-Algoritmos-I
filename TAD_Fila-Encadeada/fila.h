e7c85fa2231f2e6c35a9bd6b3d8c2935aa745279
TheDigitalPumpkin <39466847+TheDigitalPumpkin@users.noreply.github.com>
2018-06-25 20:02:31 -0300

    Update fila.h

--- TAD_Fila-Encadeada/fila.h
+++ TAD_Fila-Encadeada/fila.h
@@ -1,3 +1,4 @@
+//Dupla: Bruno Bacelar e Gabriel Lacerda
 #ifndef FILA_H
 #define FILA_H
 
@@ -5,31 +6,19 @@ typedef struct cliente
 {
     int cpf_cliente, cpf_terceiros, pri;  //P -> Prioridade.
     char bem[25], opr;
+    struct cliente* prox;
 } Cliente;
-
-typedef struct bloco
-{
-    Cliente* c; 
-    struct bloco* prox;
-} Bloco;
 
 typedef struct fila
 {
-    Bloco *ini, *fim;
+    Cliente *ini, *fim;
     int tamanho;
+    int total;
     int pr;  //pr = prioridade da fila
 } Fila;
 
-
 Fila* criar_filas(int L);
-void esvaziar_fila(Fila* f);
-void deletar_fila(Fila* f);
 int fila_cheia(Fila* f);
-int fila_vazia(Fila* f);
-int fila_tamanho(Fila* f);
-Cliente* fila_front(Fila* f, int prioridade);
-void fila_inserir(Fila* f, int cpf_c, int cpf_t, int pr, char opr);
-void fila_retirar(Fila* f);
-void imprimir_fila(Fila* f, int prioridade);
+
 
 #endif
